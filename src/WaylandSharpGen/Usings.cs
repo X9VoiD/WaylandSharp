b1d77f5f6e10f13502c22ce2c251ad669c7da345
X9VoiD <oscar.silvestrexx@gmail.com>
2022-12-21 14:33:12 +0000

    Move global usings to csproj

--- src/WaylandSharpGen/Usings.cs
+++
@@ -1,4 +0,0 @@
-global using Microsoft.CodeAnalysis;
-global using Microsoft.CodeAnalysis.CSharp;
-global using Microsoft.CodeAnalysis.CSharp.Syntax;
-global using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
